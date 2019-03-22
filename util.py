5d8eef3b315e0f524f8ef72d955552ab5c637b14
Ilya Shalnev <zeke@cs-cart.com>
2019-03-22 15:35:04 +0400

    [~] Checking for existanse of .env file was added.

--- util.py
+++ util.py
@@ -1,8 +1,10 @@
 import os
 
-filepath = os.path.dirname(os.path.realpath(__file__)) + '/.env'
-with open(filepath) as fp:
-    for cnt, line in enumerate(fp):
-        parts = line.split('=', 2)
-        if len(parts) == 2:
-            os.environ[parts[0].strip()] = parts[1].strip()
+filename = os.path.dirname(os.path.realpath(__file__)) + '/.env'
+
+if os.path.isfile(filename):
+    with open(filename) as fp:
+        for cnt, line in enumerate(fp):
+            parts = line.split('=', 2)
+            if len(parts) == 2:
+                os.environ[parts[0].strip()] = parts[1].strip()
